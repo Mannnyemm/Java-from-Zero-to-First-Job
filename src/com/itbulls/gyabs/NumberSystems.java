42071cc059faa1e3ca0663d1f94a4dd6e618992e
Mannnyemm <88416658+Mannnyemm@users.noreply.github.com>
2022-10-09 13:35:00 +0000

    Number Systems Assignment

---
+++ src/com/itbulls/gyabs/NumberSystems.java
@@ -0,0 +1,15 @@
+package com.itbulls.gyabs;
+
+public class NumberSystems {
+
+	public static void main(String[] args) {
+		/*
+		 * Hexadecimal ACDC to decimal assignment
+		 */
+		// Initialising ACDC to variable acdcdec
+		int acdcdec = 0xACDC;
+		// Displaying results: 44252
+		System.out.println(acdcdec);
+	}
+
+}
