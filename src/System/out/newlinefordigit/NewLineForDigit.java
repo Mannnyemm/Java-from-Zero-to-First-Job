a5247e1b8303f9b48166765790f954a82926e1a8
Mannnyemm <88416658+Mannnyemm@users.noreply.github.com>
2022-10-11 20:18:59 +0000

    Exercise 9
    Print all digits in user inputted integer on new lines

---
+++ src/System/out/newlinefordigit/NewLineForDigit.java
@@ -0,0 +1,23 @@
+package System.out.newlinefordigit;
+
+import java.util.Scanner;
+
+public class NewLineForDigit {
+
+	public static void main(String[] args) {
+		// Asks user to input an Integer
+		System.out.print("Please, enter any integer: ");
+		// Initialize Scanner
+		Scanner sc = new Scanner(System.in);
+		// Gets user input
+		String userInput = sc.next();
+		// Converts userInput to Array
+		String[] userInputArr = userInput.split(""); 
+		// Prints each digit in userInput on a seperate line using for each loop
+		for (String string : userInputArr) {
+			System.out.println(string);
+		}
+
+	}
+
+}
